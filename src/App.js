2d058825864fb7db9da13e81389faf5962920505
Alex Berumen <alexandroberumen@gmail.com>
2021-03-20 14:20:34 -0700

    created routing and nav

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import './App.css';
-import Hero from './Components/Hero/Hero'
+import Nav from './Components/Nav/Nav'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+import About from './Pages/About'
+import Home from './Pages/Home'
 
 function App() {
   return (
-    <div className="App">
-      <Hero />
-    </div>
+
+    <Router>
+      <div className="App">
+        <Nav />
+        <Switch>
+          <Route path='/' component={Home} exact />
+          <Route path='/about' component={About} />
+
+        </Switch>
+      </div>
+
+    </Router>
+
   );
 }
 
---
+++ src/Components/Nav/Nav.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import styled from 'styled-components'
+
+
+
+let NavContainer = styled.nav`
+width: 100%;
+height: 3rem;
+background-color: black;
+color: white;
+
+`
+
+const Nav = () => {
+    return (
+        <div>
+            <NavContainer>
+                hi
+            </NavContainer>
+
+        </div>
+    )
+}
+
+export default Nav
