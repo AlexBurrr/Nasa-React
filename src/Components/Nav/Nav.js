03c8d3f3744ff19026a71df7a567047706b018ab
Alex Berumen <alexandroberumen@gmail.com>
2021-03-21 00:04:52 -0700

    completed nav skeleton

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,21 +1,65 @@
 import React from 'react'
 import styled from 'styled-components'
+import { Link } from 'react-router-dom'
 
 
 
 let NavContainer = styled.nav`
 width: 100%;
-height: 3rem;
+height: 7rem;
 background-color: black;
 color: white;
+display:flex;
+align-items:center;
+justify-content: space-between;
+`
 
+let NavLogo = styled.div`
+
+`
+
+let ListContainer = styled.ul`
+    display:flex;
+
+`
+
+let ListItem = styled.li`
+    list-style: none;
+    padding:1rem;
+
+    &:hover{
+        opacity:.5;
+    }
 `
 
 const Nav = () => {
     return (
         <div>
             <NavContainer>
-                hi
+                <NavLogo>
+                    <Link to='/'>
+                        <ListItem>
+                            NASA
+                         </ListItem>
+                    </Link>
+
+                </NavLogo>
+                <ListContainer>
+                    <Link to='/about'>
+                        <ListItem>
+                            About
+                         </ListItem>
+                    </Link>
+
+                    <Link to='/gallery'>
+                        <ListItem>
+                            Gallery
+                       </ListItem>
+
+                    </Link>
+
+
+                </ListContainer>
             </NavContainer>
 
         </div>
