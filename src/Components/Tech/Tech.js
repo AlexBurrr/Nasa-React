4d639168ac708b537dcb0d5b6f9a3d77d345b788
Alex Berumen <alexandroberumen@gmail.com>
2021-03-23 21:09:00 -0700

    figured out how t0 get object values into a array then map it

--- src/Components/Tech/Tech.js
+++ src/Components/Tech/Tech.js
@@ -11,7 +11,9 @@ today = yyyy + '-' + mm + '-' + dd
 
 const Tech = () => {
 
-    const [asteroid, setAsteroid] = useState({})
+    const [asteroid, setAsteroid] = useState('');
+    const [name, setName] = useState('')
+
     useEffect(() => {
         axios.get(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${today}&api_key=rpmjI7MRmhGa7jHTtj1zxa7B8oX5H2cyhgSmAGoK`)
             .then(res => {
@@ -21,19 +23,31 @@ const Tech = () => {
                 console.log(err);
             })
     }, [])
-
+    let astName = []
+    console.log(asteroid);
     for (const item in asteroid) {
-        let thing = (asteroid[item])
-        thing.slice(0).map((item) => {
+        let thing = (asteroid[item]).slice(0)
+        thing.slice(0).map((item, index) => {
             if (item.is_potentially_hazardous_asteroid === true) {
-                return (item.name);
+                astName.push(item.name)
             }
 
-        })
+        }, [])
     }
+
+    console.log(astName);
 
     return (
         <div>
+            {
+                astName.map((name, index) => {
+                    return (
+                        <p key={index}>
+                            {name}
+                        </p>
+                    )
+                })
+            }
 
 
         </div>
